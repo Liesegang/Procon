722042b0703a8dfd1047cd761cc953624ee1e58d
liesegang <yyama0704@gmail.com>
2020-04-17 06:17:06 +0900

    solve abc145/d

---
+++ AtCoder/abc145/d.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type alias
+using ll=long long;
+using ld=double;
+
+using pi=pair<int,int>;
+using pll=pair<ll,ll>;
+using pld=pair<ld,ld>;
+using ti3=tuple<int, int, int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vld=vector<ld>;
+using vpi=vector<pi>;
+using vpll=vector<ll>;
+using vpld=vector<ld>;
+using vti3=vector<ti3>;
+
+// Constants
+const double EPS = 1e-9;
+const int inf = numeric_limits<int>::max();
+
+// Macro
+#define rep(i,n) for(int i=0;i<int(n);++i)
+#define rrep(i,n) for(int i=int(n-1);i>=0;--i)
+#define REP(i,a,b) for(int i=int(a);i<int(b);++i)
+#define RREP(i,a,b) for(int i=int(b-1);i>=int(a);--i)
+#define SHOW(a) cout << #a << " = " << a << endl
+#define ARR(a,n) for(int i=0;i<int(n);++i) cout << #a << "[" << i << "]" << " = " << a[i] << endl
+
+#define ALL(a) a.begin(),a.end()
+
+// Funtcions
+ll pow(ll base, ll i, ll mod){
+  ll a = 1;
+  while(i){
+    if (i & 1) {
+      a *= base;
+      a %= mod;
+    }
+    base *= base;
+    base %= mod;
+    i >>= 1;
+  }
+  return a;
+}
+ll gcd(ll a, ll b){
+  while(b){
+    ll c = a % b;
+    a = b;
+    b = c;
+  }
+  return a;
+}
+ll lcm(ll a, ll b){
+  return a / gcd(a, b) * b;
+}
+template<typename T> bool chmin(T& a, const T& b) {
+  if (a > b) return a = b, true; else return false;
+}
+
+template<typename T> bool chmax(T& a, const T& b) {
+  if (a < b) return a = b, true; else return false;
+}
+
+ll ncm(ll n, ll m, ll mod){
+  ll ans = 1;
+  rep(i, m){
+    ans *= (n-i);
+    ans %= mod;
+    ans *= pow(i+1, mod - 2, mod);
+    ans %= mod;
+  }
+  return ans;
+}
+
+void solve(){
+  const ll mod = 1000000000 + 7;
+  int X, Y;
+  cin >> X >> Y;
+
+  int a = 2*X - Y;
+  int b = 2*Y - X;
+  if(a < 0 || b < 0){
+    cout << 0 << endl;
+    return;
+  }
+  if(a % 3 != 0 || b % 3 != 0){
+    cout << 0 << endl;
+    return;
+  }
+
+  a /= 3;
+  b /= 3;
+
+  cout << ncm(a + b, b, mod) << endl;
+}
+
+int main(){
+// FastIO
+//ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+  solve();
+  return 0;
+}
