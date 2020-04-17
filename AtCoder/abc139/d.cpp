b7a324942688d355285e3f319a802d4dab8d3d9b
liesegang <yyama0704@gmail.com>
2020-04-18 02:48:58 +0900

    solve abc139/d

--- AtCoder/abc139/d.cpp
+++ AtCoder/abc139/d.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 // Type alias
 using ll=long long;
 using ld=double;
- 
+
 using pi=pair<int,int>;
 using pll=pair<ll,ll>;
 using pld=pair<ld,ld>;
 using ti3=tuple<int, int, int>;
- 
+
 using vi=vector<int>;
 using vll=vector<ll>;
 using vld=vector<ld>;
@@ -17,11 +17,12 @@ using vpi=vector<pi>;
 using vpll=vector<ll>;
 using vpld=vector<ld>;
 using vti3=vector<ti3>;
- 
+
 // Constants
 const double EPS = 1e-9;
-const int inf = numeric_limits<int>::max();
- 
+const int inf = numeric_limits<int>::max() / 2;
+const ll mod = 1e9+7;
+
 // Macro
 #define rep(i,n) for(int i=0;i<int(n);++i)
 #define rrep(i,n) for(int i=int(n-1);i>=0;--i)
@@ -29,9 +30,9 @@ const int inf = numeric_limits<int>::max();
 #define RREP(i,a,b) for(int i=int(b-1);i>=int(a);--i)
 #define SHOW(a) cout << #a << " = " << a << endl
 #define ARR(a,n) for(int i=0;i<int(n);++i) cout << #a << "[" << i << "]" << " = " << a[i] << endl
- 
+
 #define ALL(a) a.begin(),a.end()
- 
+
 // Funtcions
 ll pow(ll base, ll i, ll mod){
   ll a = 1;
@@ -57,35 +58,26 @@ ll gcd(ll a, ll b){
 ll lcm(ll a, ll b){
   return a / gcd(a, b) * b;
 }
- 
- 
-void solve(){
-
+template<typename T> bool chmin(T& a, const T& b) {
+  if (a > b) return a = b, true; else return false;
 }
- 
+
+template<typename T> bool chmax(T& a, const T& b) {
+  if (a < b) return a = b, true; else return false;
+}
+
+
+void solve(){
+  ll N;
+  cin >> N;
+
+  cout << N * (N - 1LL) / 2LL << endl;
+}
+
 int main(){
-  // FastIO
-  //ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+// FastIO
+//ios_base::sync_with_stdio(false);
+cin.tie(NULL);
   solve();
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
