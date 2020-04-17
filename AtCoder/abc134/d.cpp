2180645786d748bee0595ecacff1ab9af936c2f6
liesegang <yyama0704@gmail.com>
2020-04-18 03:58:54 +0900

    solve abc134/d

---
+++ AtCoder/abc134/d.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type alias
+using ll=long long;
+using ld=double;
+
+using pi=pair<int,int>;
+using pll=pair<ll,ll>;
+using pld=pair<ld,ld>;
+using ti3=tuple<int, int, int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vld=vector<ld>;
+using vpi=vector<pi>;
+using vpll=vector<ll>;
+using vpld=vector<ld>;
+using vti3=vector<ti3>;
+
+// Constants
+const double EPS = 1e-9;
+const int inf = numeric_limits<int>::max() / 2;
+const ll mod = 1e9+7;
+
+// Macro
+#define rep(i,n) for(int i=0;i<int(n);++i)
+#define rrep(i,n) for(int i=int(n-1);i>=0;--i)
+#define REP(i,a,b) for(int i=int(a);i<int(b);++i)
+#define RREP(i,a,b) for(int i=int(b-1);i>=int(a);--i)
+#define SHOW(a) cout << #a << " = " << a << endl
+#define ARR(a,n) for(int i=0;i<int(n);++i) cout << #a << "[" << i << "]" << " = " << a[i] << endl
+
+#define ALL(a) a.begin(),a.end()
+
+// Funtcions
+ll pow(ll base, ll i, ll mod){
+  ll a = 1;
+  while(i){
+    if (i & 1) {
+      a *= base;
+      a %= mod;
+    }
+    base *= base;
+    base %= mod;
+    i >>= 1;
+  }
+  return a;
+}
+ll gcd(ll a, ll b){
+  while(b){
+    ll c = a % b;
+    a = b;
+    b = c;
+  }
+  return a;
+}
+ll lcm(ll a, ll b){
+  return a / gcd(a, b) * b;
+}
+template<typename T> bool chmin(T& a, const T& b) {
+  if (a > b) return a = b, true; else return false;
+}
+
+template<typename T> bool chmax(T& a, const T& b) {
+  if (a < b) return a = b, true; else return false;
+}
+
+
+void solve(){
+  int N;
+  cin >> N;
+
+  int a[200000 + 5];
+  rep(i, N) cin >> a[i+1];
+
+  int ball[200000+5] = {};
+  RREP(i, 1, N+1){
+    int ans = 0;
+    for(int j = 2 * i; j <= N; j += i){
+      ans += ball[j];
+    }
+    if(ans % 2 != a[i])
+      ball[i] = 1;
+  }
+
+  int m = 0;
+  REP(i, 1, N+1){
+    if(ball[i]) m++;
+  }
+  cout << m << endl;
+  REP(i, 1, N+1){
+    if(ball[i]) cout << i << " ";
+  }
+  cout << endl;
+}
+
+int main(){
+// FastIO
+//ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+  solve();
+  return 0;
+}
