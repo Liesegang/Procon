f9837428ee14ce2ade4f65c891e4eb08c0f90fc1
liesegang <yyama0704@gmail.com>
2020-04-18 02:35:51 +0900

    update Library/number format, efficience

--- Library/number.cpp
+++ Library/number.cpp
@@ -1,10 +1,10 @@
 // エラトステネスの篩
 // 素数列挙
 
-std::vector<int> Eratosthenes(const int N){
-  std::vector<bool> is_prime(N + 1);
+vector<int> Eratosthenes(const int N){
+  vector<bool> is_prime(N + 1);
   for(int i = 0; i <= N; i++) is_prime[i] = true;
-  std::vector<int> P;
+  vector<int> P;
   for(int i = 2; i <= N; i++){
     if(is_prime[i]){
       for(int j = 2 * i; j <= N; j += i) is_prime[j] = false;
@@ -16,21 +16,28 @@ std::vector<int> Eratosthenes(const int N){
 
 
 // 素数判定
-bool IsPrime(int num){
+bool IsPrime(ll num){
   if(num < 2) return false;
   if(num == 2) return true;
   if(num % 2 == 0) return false;
 
   double sqrtNum = sqrt(num);
-  for(int i = 3; i <= sqrtNum; i += 2) if(num % i == 0) return false;
+  for(ll i = 3; i <= sqrtNum; i += 2) if(num % i == 0) return false;
   return true;
 }
 
 
 // 素因数分解
-map< ll, int > PrimeFactor(ll n) {
-  map< ll, int > ret;
-  for(ll i = 2; i * i <= n; i++){
+map<ll, int> PrimeFactor(ll n) {
+  map<ll, int> ret;
+
+  while(n % 2 == 0){
+    ret[2]++;
+    n /= 2;
+  }
+
+  double sqrtNum = sqrt(num);
+  for(ll i = 3; i <= sqrtNum; i += 2){
     while(n % i == 0){
       ret[i]++;
       n /= i;
