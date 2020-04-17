2fbb68b6f1ed9bff61887d8cd9c40d5c84a53257
liesegang <yyama0704@gmail.com>
2020-04-18 01:50:12 +0900

    update Library/number PrimeFactor,IsPrime:format, PrimeFactor:efficient algorithm

--- Library/number.cpp
+++ Library/number.cpp
@@ -17,12 +17,12 @@ std::vector<int> Eratosthenes(const int N){
 
 // 素数判定
 bool IsPrime(int num){
-  if (num < 2) return false;
-  else if (num == 2) return true;
-  else if (num % 2 == 0) return false;
+  if(num < 2) return false;
+  if(num == 2) return true;
+  if(num % 2 == 0) return false;
 
   double sqrtNum = sqrt(num);
-  for (int i = 3; i <= sqrtNum; i += 2) if (num % i == 0) return false;
+  for(int i = 3; i <= sqrtNum; i += 2) if(num % i == 0) return false;
   return true;
 }
 
