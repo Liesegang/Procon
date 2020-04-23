cb09a663ebab3e94fd54f510c1ad591d0f7f207b
liesegang <yyama0704@gmail.com>
2020-04-24 06:27:58 +0900

    solve dp/c

---
+++ AtCoder/dp/c.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type alias
+using ll=long long;
+using ld=double;
+
+using pi=pair<int,int>;
+using pll=pair<ll,ll>;
+using pld=pair<ld,ld>;
+using ti3=tuple<int, int, int>;
+
+using vi=vector<int>;
+using vll=vector<ll>;
+using vld=vector<ld>;
+using vpi=vector<pi>;
+using vpll=vector<ll>;
+using vpld=vector<ld>;
+using vti3=vector<ti3>;
+
+// Constants
+const double EPS = 1e-9;
+const int inf = numeric_limits<int>::max() / 2;
+const ll mod = 1e9+7;
+
+// Macro
+#define rep(i,n) for(int i=0;i<int(n);++i)
+#define rrep(i,n) for(int i=int(n-1);i>=0;--i)
+#define REP(i,a,b) for(int i=int(a);i<int(b);++i)
+#define RREP(i,a,b) for(int i=int(b-1);i>=int(a);--i)
+#define SHOW(a) cout << #a << " = " << a << endl
+#define ARR(a,n) for(int i=0;i<int(n);++i) cout << #a << "[" << i << "]" << " = " << a[i] << endl
+
+#define ALL(a) a.begin(),a.end()
+
+// Funtcions
+ll pow(ll base, ll i, ll mod){
+  ll a = 1;
+  while(i){
+    if (i & 1) {
+      a *= base;
+      a %= mod;
+    }
+    base *= base;
+    base %= mod;
+    i >>= 1;
+  }
+  return a;
+}
+ll gcd(ll a, ll b){
+  while(b){
+    ll c = a % b;
+    a = b;
+    b = c;
+  }
+  return a;
+}
+ll lcm(ll a, ll b){
+  return a / gcd(a, b) * b;
+}
+template<typename T> bool chmin(T& a, const T& b) {
+  if (a > b) return a = b, true; else return false;
+}
+
+template<typename T> bool chmax(T& a, const T& b) {
+  if (a < b) return a = b, true; else return false;
+}
+
+int a[100000+5], b[100000+5], c[100000+5];
+ll h[3][100000+5];
+void solve(){
+  int N; cin >> N;
+  REP(i, 0, N) cin >> a[i] >> b[i] >> c[i];
+
+  h[0][0] = a[0];
+  h[1][0] = b[0];
+  h[2][0] = c[0];
+  REP(i, 1, N){
+    h[0][i] = max(h[1][i-1] + a[i], h[2][i-1] + a[i]);
+    h[1][i] = max(h[2][i-1] + b[i], h[0][i-1] + b[i]);
+    h[2][i] = max(h[0][i-1] + c[i], h[1][i-1] + c[i]);
+  }
+  cout << max(h[0][N-1], max(h[1][N-1], h[2][N-1])) << endl;
+}
+
+int main(){
+// FastIO
+//ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+  solve();
+  return 0;
+}
